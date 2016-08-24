5fae6f81abc3ce2a1c2b7ba426e468b0a43e239c
Cormick <cvhnilicka@wisc.edu>
2016-08-24 18:20:57 -0500

    adding to another computer

--- server.js
+++ server.js
@@ -8,6 +8,60 @@ var database = require('./config/database'); 			// load the database config
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var vpnc = require('vpnc');
+
+var config = {
+    IPSec_ID: 'WiscVPN-OnCampus',
+    IPSec_gateway: 'wiscvpn.doit.wisc.edu',
+    IPSec_secret: 'vpnw1sc2002',
+
+    Xauth_username: 'cvhnilicka',
+    Xauth_password: 'Busterboca3',
+
+    IKE_Authmode: 'psk',
+    IKE_DH_Group: 'WiscVPN-OnCampus',
+    DNSUpdate: 'no',
+    NAT_Traversal_Mode: 'force-natt',
+    Local_Port: 0,
+    Cisco_UDP_Encapsulation_Port: 0,
+};
+
+
+console.log("vpnc", vpnc)
+
+vpnc.available(function (err, version) {
+    if (err) {
+        console.log('VPN unavailable:');
+        console.log(err);
+    } else {
+        console.log('Found ' + version);
+        connect();
+    }
+});
+
+function connect() {
+    vpnc.connect(config, function (err, code) {
+        if (err) {
+            console.log('Error connecting VPN:');
+            console.log(err);
+        } else {
+            console.log('VPN connected. Disconnecting in five seconds.');
+            setTimeout(disconnect, 5000);
+        }
+    });
+}
+
+function disconnect() {
+    vpnc.disconnect(function (err, code) {
+        if (err) {
+            console.log('Error disconnecting VPN:');
+            console.log(err);
+        } else {
+            console.log('VPN disconnected.');
+        }
+    });
+}
+
 
 // configuration ===============================================================
 mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
